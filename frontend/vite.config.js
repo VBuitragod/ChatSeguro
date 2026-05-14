f750d49feec036a823f03d4bdb87c73e5b892549
Vbuitragod <buitragov689@gmail.com>
2026-05-13 21:56:26 -0500

    correcciones

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-});
+  // Esto ayuda a que Vercel encuentre los archivos en la raíz
+  base: '/', 
+})
